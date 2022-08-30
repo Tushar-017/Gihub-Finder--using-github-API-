d2f9426520eba2fda6766a8728b5721c1218530e
Tushar Rajput <tusharo17rajput@gmail.com>
2022-08-30 09:36:59 +0530

    initial commit for github finder with react context

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Notfound from './pages/Notfound';
+import { GithubProvider } from './components/context/github/GithubContext';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GithubProvider>
+      <BrowserRouter>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar/>
+
+          <main className='container mx-auto px-3 pb-12'>
+            <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/about' element={<About/>}/>
+              <Route path='/notfound' element={<Notfound/>}/>
+              <Route path='/*' element={<Notfound/>}/>
+            </Routes>
+          </main>
+
+          <Footer/>
+        </div>
+      </BrowserRouter>
+    </GithubProvider>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import UserResults from "../components/users/UserResults"
+
+function Home() {
+  return (
+    <>
+      {/* search comp */}
+      <UserResults/>
+    </>
+  )
+}
+
+export default Home
